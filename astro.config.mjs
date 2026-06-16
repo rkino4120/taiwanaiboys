5ea2378bd017b59d4c45ed3704d93a5e6820b0ce
rkino4120 <rkino4120@gmail.com>
2026-06-16 23:40:53 +0900

    公開用調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
@@ -9,32 +9,6 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   integrations: [react()],
-  fonts: [
-    {
-      provider: fontProviders.google(),
-      name: "Sawarabi Gothic",
-      cssVariable: "--font-sawarabi-gothic",
-      subsets: ["latin", "japanese"],
-    },
-    {
-      provider: fontProviders.google(),
-      name: "Shippori Mincho",
-      cssVariable: "--font-shippori-mincho",
-      subsets: ["latin", "japanese"],
-    },
-    {
-      provider: fontProviders.google(),
-      name: "Noto Sans TC",
-      cssVariable: "--font-noto-sans-tc",
-      subsets: ["latin", "chinese-traditional"],
-    },
-    {
-      provider: fontProviders.google(),
-      name: "Noto Serif TC",
-      cssVariable: "--font-noto-serif-tc",
-      subsets: ["latin", "chinese-traditional"],
-    },
-  ],
   vite: {
     plugins: [tailwindcss()],
     ssr: {
